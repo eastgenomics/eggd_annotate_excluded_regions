295c97a77351b89dcdf878622af48ae3d35422b6
AishaD <AishaDahir75@gmail.com>
2022-08-23 10:02:32 +0100

    GCNV-31 handle optional panel file input in json & bash

--- src/eggd_annotate_excluded_regions.sh
+++ src/eggd_annotate_excluded_regions.sh
@@ -11,22 +11,29 @@ main() {
 	pip3 install /pytz-*.whl /numpy-*.whl /pandas-*.whl
 
     echo "--------------Filtering and annotating excluded files -----------------"
+    # sometimes a panel bed file is not provided
+    if [ -z "$panel_bed" ]; then
+        echo "No panel bed file provided, so the gCNV excluded regions is annotated."
+        bedtools intersect -b $exons_hgnc_path -a $excluded_regions_path -wao > excluded_genes.bed
+        head excluded_genes.bed
+        python3 annotate_excluded_panel.py -e excluded_genes.bed -r $excluded_regions_path -g $exons_gene_path
 
-    bedtools intersect -a $excluded_regions_path -b $panel_bed_path | sort | uniq > panel_excluded.bed
-    head panel_excluded.bed
-    # some panels may not be excluded so the panel_excluded.bed so have
-    # an empty file outputted here.
-
-    if [ -s panel_excluded.bed ]; then
-        echo "Some panel regions over lap with the gCNV excluded regions, these will be annotated."
-        bedtools intersect -b $exons_hgnc_path -a panel_excluded.bed -wao > panel_excluded_genes.bed
-        head panel_excluded_genes.bed
-        python3 annotate_excluded_panel.py -e panel_excluded_genes.bed -p $panel_bed_path -r $excluded_regions_path -g $exons_gene_path
     else
-		echo "Panel regions do not overlap with cnv calling excluded regions."
-        printf "Chrom\tStart\tEnd\tHGNC_ID\tTranscript\tExon\n" | tee touch $(echo ${excluded_regions_path##*/} | sed 's/.bed//g')_${panel_bed_path##*/}
-	fi
-
+        echo "Panel bed file is provided, so the gCNV excluded regions will be interested with panel bed file."
+        bedtools intersect -a $excluded_regions_path -b $panel_bed_path | sort | uniq > panel_excluded.bed
+        head panel_excluded.bed
+        # some panels may not be excluded so the panel_excluded.bed so have
+        # an empty file outputted here.
+        if [ -s panel_excluded.bed ]; then
+            echo "Some panel regions over lap with the gCNV excluded regions, these will be annotated."
+            bedtools intersect -b $exons_hgnc_path -a panel_excluded.bed -wao > panel_excluded_genes.bed
+            head panel_excluded_genes.bed
+            python3 annotate_excluded_panel.py -e panel_excluded_genes.bed -p $panel_bed_path -r $excluded_regions_path -g $exons_gene_path
+        else
+            echo "Panel regions do not overlap with cnv calling excluded regions."
+            printf "Chrom\tStart\tEnd\tHGNC_ID\tTranscript\tExon\n" | tee touch $(echo ${excluded_regions_path##*/} | sed 's/.bed//g')_${panel_bed_path##*/}
+        fi
+    fi
 
     echo "--------------Outputting files -----------------"
     mkdir -p /home/dnanexus/out/annotated_excluded_file/
